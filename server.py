ec4a797af2a770618bc0b4def083dd32eb779b28
doraKoreny <dorakoreny@gmail.com>
2018-07-24 13:22:56 +0200

    display question function route implemented

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-import data_manager
+import connection
 
 app = Flask(__name__)
 
@@ -8,12 +8,21 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def route_home():
-    questions = data_manager.get_all_question()
+    questions = connection.get_all_question()
     questions = sorted(questions, key=lambda x: x['submission_time'], reverse=True)
-    # @app.route('/question/<question_id>')
-
-    # @app.route('/add_question')
     return render_template('list.html', questions=questions)
+
+@app.route('/question/<question_id>', methods=['GET', 'POST'])
+def display_question(question_id):
+    questions = connection.get_all_question()
+    for item in questions:
+        if item['id'] == str(question_id):
+            details = item
+    data = {'id': details['id'], 'submission_time':details['submission_time'],'view_number':details['view_number'],'vote_number':details['vote_number'],'title':details['title'],'message':details['message'],'image':details['image']}
+    return render_template('question.html', data=data)
+    
+    
+    # @app.route('/add_question')
 
 
 if __name__ == '__main__':
