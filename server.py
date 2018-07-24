485a245ebeb1a089321c46c5b7a1a547cae2dd1d
doraKoreny <dorakoreny@gmail.com>
2018-07-24 14:15:17 +0200

    added display_answers functions for question/question id route

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 
 import connection
+import time
 
 app = Flask(__name__)
 
@@ -18,11 +19,20 @@ def display_question(question_id):
     for item in questions:
         if item['id'] == str(question_id):
             details = item
-    data = {'id': details['id'], 'submission_time':details['submission_time'],'view_number':details['view_number'],'vote_number':details['vote_number'],'title':details['title'],'message':details['message'],'image':details['image']}
-    return render_template('question.html', data=data)
-    
-    
-    # @app.route('/add_question')
+    data_question = {'id': details['id'], 'submission_time':time.ctime(int(details['submission_time'])),'view_number':details['view_number'],
+            'vote_number':details['vote_number'],'title':details['title'],'message':details['message'],
+            'image':details['image']}
+    return render_template('question.html', data_question=data_question)
+
+@app.route('/question/<question_id>', methods=['GET', 'POST'])
+def display_answer(question_id):
+    answers = connection.get_all_answer()
+    answers_for_question = []
+    for item in answers:
+        if item['question_id'] == str(question_id):
+            answers_for_question.append(item)
+    return render_template('question.html', answers_for_question=answers_for_question)
+
 
 
 if __name__ == '__main__':
