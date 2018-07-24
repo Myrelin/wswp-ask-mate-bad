6cf7957f0591bbfe991a415255b110d39fa7a504
Boldizsar Balogh <bal.boldizsar@gmail.com>
2018-07-24 11:26:30 +0200

    implemented the get all questions function

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,15 @@
 import os
+import csv
 
-QUESTION_DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'question.csv'
-ANSWER_DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'answer.csv'
+QUESTION_DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/question.csv'
+ANSWER_DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else 'sample_data/answer.csv'
+
+
+def get_all_question():
+    with open(QUESTION_DATA_FILE_PATH) as csvfile:
+        reader = csv.DictReader(csvfile)
+        all_questions = list(reader)
+        return all_questions
+
+
+get_all_question()
