114f3fc72bf429e40449ba9bc7ef2f5f1c67deca
Boldizsar Balogh <bal.boldizsar@gmail.com>
2018-07-24 11:27:40 +0200

    implemented server side logic to questions and render_template home.html

--- server.py
+++ server.py
@@ -1,19 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
+import data_manager
 
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/list')
+def route_home():
+    questions = data_manager.get_all_question()
+    questions = sorted(questions, key=lambda x: x['submission_time'], reverse=True)
+    # @app.route('/question/<question_id>')
 
-
-@app.route('/question/<question_id>')
-
-
-@app.route('/add_question')
-
-
+    # @app.route('/add_question')
+    return render_template('list.html', questions=questions)
 
 
 if __name__ == '__main__':
