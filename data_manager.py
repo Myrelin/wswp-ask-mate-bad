df301ba8d4e5ecd62fc6c07afed43cbf828d59a7
doraKoreny <dorakoreny@gmail.com>
2018-07-25 13:03:43 +0200

    add_answer function implemented

--- data_manager.py
+++ data_manager.py
@@ -16,6 +16,21 @@ def add_question(data):
     connection.write_new_answer(record_to_add)
 
 
+def add_answer(data):
+    answers = connection.get_all_answer()
+    try:
+        new_id = str(int(answers[-1]['id']) + 1)
+    except IndexError:
+        new_id = '1'
+    record_to_add = data
+    record_to_add['id'] = new_id
+    record_to_add['submission_time'] = str(int(round(time.time())))
+    record_to_add['view_number'] = '0'
+    record_to_add['vote_number'] = '0'
+    connection.write_new_answer(record_to_add)
+
+
+
 def get_all_question():
     questions = connection.get_all_question()
     questions = sorted(questions, key=lambda x: x['submission_time'], reverse=True)
