ecd5abbc39f7e65f86e2e678fe41df0bc0dda5c2
doraKoreny <dorakoreny@gmail.com>
2018-07-25 14:39:16 +0200

    delete_questions function is implemented

--- data_manager.py
+++ data_manager.py
@@ -38,6 +38,9 @@ def get_all_question():
     questions = sorted(questions, key=lambda x: x['submission_time'], reverse=True)
     return questions
 
+def get_all_answers():
+    answers = connection.get_all_answer()
+    return answers
 
 def get_question_by_id(data, question_id):
     for item in data:
@@ -51,7 +54,7 @@ def convert_timestamp(data):
     return data
 
 
-def get_all_answer(question_id):
+def get_answers_for_question(question_id):
     answers = connection.get_all_answer()
     answers_for_question = []
     for item in answers:
@@ -59,3 +62,15 @@ def get_all_answer(question_id):
             item = convert_timestamp(item)
             answers_for_question.append(item)
     return answers_for_question
+
+def delete_questions(question_id):
+    questions = get_all_question()
+    answers = get_all_answers()
+    for question in questions:
+        if question['id'] == question_id:
+            del question
+    for answer in answers:
+        if answer['question_id'] == question_id:
+            del answer
+    connection.write_data(questions, DATA_HEADER_Q)
+    connection.write_data(answers, DATA_HEADER_A,False)
