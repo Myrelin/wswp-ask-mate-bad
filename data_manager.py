f428aae3f6fa974278b600cbee15f1c631b27237
Boldizsar Balogh <bal.boldizsar@gmail.com>
2018-07-25 10:53:20 +0200

    implemented data handling functionality from server

--- data_manager.py
+++ data_manager.py
@@ -14,3 +14,31 @@ def add_question(data):
     record_to_add['view_number'] = '0'
     record_to_add['vote_number'] = '0'
     connection.write_new_answer(record_to_add)
+
+
+def get_all_question():
+    questions = connection.get_all_question()
+    questions = sorted(questions, key=lambda x: x['submission_time'], reverse=True)
+    return questions
+
+
+def get_question_by_id(data, question_id):
+    for item in data:
+        if item['id'] == str(question_id):
+            data_question = item
+    return data_question
+
+
+def convert_timestamp(data):
+    data['submission_time'] = time.ctime(int(data['submission_time']))
+    return data
+
+
+def get_all_answer(question_id):
+    answers = connection.get_all_answer()
+    answers_for_question = []
+    for item in answers:
+        if item['question_id'] == str(question_id):
+            item = convert_timestamp(item)
+            answers_for_question.append(item)
+    return answers_for_question
